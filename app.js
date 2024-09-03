75645962441103a7f69d6c5c41272ae691ec2f29
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-09-03 20:44:03 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.set("view engine", "ejs")
+app.set("views", "views")
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(8000, () => console.log("server started"))
