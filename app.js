9c4a2ff73f902e534951d70ae2d98a47452c8b69
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-09-04 19:37:43 +0200

    add form

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs")
 app.set("views", "views")
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render('index')
 })
 
 app.listen(8000, () => console.log("server started"))
