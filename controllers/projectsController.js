49bba23f91b182eaf4696b02160b472928d0aeee
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-09-07 19:55:46 +0200

    change queries

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,8 +1,10 @@
 import db from '../db/queries.js'
 
 async function getProjects(req, res) {
-    const projects = await db.getAllProjects()
-    res.render('index', {projects: projects})
+    const { interest, skillLevel, techStack } = req.query;
+    console.log(skillLevel)
+    const projects = await db.getFilteredProjects({ interest, skillLevel, techStack });
+    res.render('index', { projects, interest, skillLevel, techStack })
 }
 
 export default { getProjects }
--- db/queries.js
+++ db/queries.js
@@ -1,8 +1,26 @@
 import pool from "./pool.js";
 
-async function getAllProjects() {
-    const { rows } = await pool.query("SELECT * FROM projects")
-    return rows
+async function getFilteredProjects(filters) {
+    let query = 'SELECT * FROM projects WHERE 1=1';
+    const values = [];
+
+    // Append filters to the query
+    if (filters.interest) {
+        query += ' AND interest = $1';
+        values.push(filters.interest);
+    }
+    if (filters.skillLevel) {
+        query += ' AND skill_level = $2';
+        values.push(filters.skillLevel);
+    }
+    if (filters.techStack) {
+        query += ' AND technologies ILIKE $3'; // Assuming tech_stack is a text field and using ILIKE for partial matching
+        values.push(`%${filters.techStack}%`);
+    }
+
+    const { rows } = await pool.query(query, values);
+    return rows;
 }
 
-export default { getAllProjects }
+export default { getFilteredProjects };
+
