d5dfe2e2d3bd8db947027f759c1e0bf117af08e9
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-09-10 19:01:18 +0200

    change UI header

--- db/queries.js
+++ db/queries.js
@@ -3,19 +3,18 @@ import pool from "./pool.js";
 async function getFilteredProjects(filters) {
     let query = 'SELECT * FROM projects WHERE 1=1';
     const values = [];
-    let count = 1
+    let param = 1
 
     // Append filters to the query
     if (filters.interest) {
-        console.log(filters.interest)
-        query += ` AND interest = $${count}`;
+        query += ` AND interest = $${param}`;
         values.push(filters.interest);
-        count++
+        param++
     }
     if (filters.skillLevel) {
-        query += ` AND skill_level = $${count}`;
+        query += ` AND skill_level = $${param}`;
         values.push(filters.skillLevel);
-        count++
+        param++
     }
 
     const { rows } = await pool.query(query, values);
