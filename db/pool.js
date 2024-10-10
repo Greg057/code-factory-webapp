2c36d7f28e97f8fae68e30eb5e4e0583a27cee3a
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-10-10 18:29:30 +0200

    Updated database connection for Heroku

--- db/pool.js
+++ db/pool.js
@@ -3,5 +3,12 @@ import 'dotenv/config'
 const { Pool } = pkg;
 
 export default new Pool({
-    connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DB_PORT}/${process.env.DATABASE}`
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
+// export default new Pool({
+//     connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DB_PORT}/${process.env.DATABASE}`
+// });
