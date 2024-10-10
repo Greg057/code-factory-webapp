693bce6dfc82b6c90c0eb46d86e852097ff3cbd5
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-10-10 19:00:56 +0200

    Redirect HTTP to HTTPS in production

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ import pageRouter from './routes/pageRouter.js'
 
 const app = express()
 
+// Redirect HTTP to HTTPS in production
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(`https://${req.headers.host}${req.url}`);
+        }
+        next();
+    });
+}
+
 app.use(express.static('public'))
 
 app.set("view engine", "ejs")
