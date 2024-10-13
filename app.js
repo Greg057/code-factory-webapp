3a2801c4d823bc5086f8366d2f6b930d8109bc9c
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-10-13 12:03:04 +0200

    improved security

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-import express from 'express'
-import 'dotenv/config'
-import projectsRouter from './routes/projectsRouter.js'
-import pageRouter from './routes/pageRouter.js'
-import sitemapRouter from './routes/sitemapRouter.js'
-
+import express from 'express';
+import 'dotenv/config';
+import projectsRouter from './routes/projectsRouter.js';
+import pageRouter from './routes/pageRouter.js';
+import sitemapRouter from './routes/sitemapRouter.js';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const app = express()
 app.set('etag', 'strong');
-
-
 
 // Redirect HTTP to HTTPS in production
 if (process.env.NODE_ENV === 'production') {
@@ -22,6 +21,35 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(express.static('public'));
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "trusted-cdn.com"],
+        styleSrc: ["'self'", "trusted-cdn.com"],
+        imgSrc: ["'self'", "trusted-cdn.com"],
+        connectSrc: ["'self'"],
+        fontSrc: ["'self'", "trusted-cdn.com"],
+        objectSrc: ["'none'"],
+        mediaSrc: ["'self'"],
+        frameSrc: ["'none'"]
+    }
+}));
+
+app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    next();
+});
+
 
 app.set("view engine", "ejs")
 app.set("views", "views")
@@ -29,8 +57,16 @@ app.set("views", "views")
 app.use("/", projectsRouter)
 app.use("/sitemap.xml", sitemapRouter);
 app.use("/page/", pageRouter)
+
+// 404 handler
 app.use((req, res) => {
     res.status(404).render('404');
+});
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
 
 const port = process.env.PORT || 3000;
